e445a7c11841a00163b0b5a289354321c8c09230
Albert Llopart Navarra <d.pocafeiner@gmail.com>
2017-11-14 11:44:12 +0100

    added files

---
+++ ModuleSceneIntro.cpp
@@ -0,0 +1,67 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleSceneIntro.h"
+#include "Primitive.h"
+#include "PhysBody3D.h"
+
+ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleSceneIntro::~ModuleSceneIntro()
+{}
+
+// Load assets
+bool ModuleSceneIntro::Start()
+{
+	LOG("Loading Intro assets");
+	bool ret = true;
+
+	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
+	App->camera->LookAt(vec3(0, 0, 0));
+
+	// TODO 3: create a sphere in the world with a primitive
+	// and create a physics body for it. Remember to render it in Update()
+	btCollisionShape* colShape = new btSphereShape(1.0f);
+	mat4x4 trans = IdentityMatrix;
+	vec3 pos = (0, 1, 0);
+	trans.translate(pos.x, pos.y, pos.z);
+	btTransform transform;
+	transform.setFromOpenGLMatrix(&trans);
+	btDefaultMotionState* myMotionState = new btDefaultMotionState(transform);
+	btRigidBody::btRigidBodyConstructionInfo rbInfo(1.0f, myMotionState, colShape);
+	btRigidBody* body = new btRigidBody(rbInfo);
+
+	App->physics->world->addRigidBody(body);
+	b = new PhysBody3D(body);
+
+	// TODO 5: Add this module to the list of collision listeners
+
+	return ret;
+}
+
+// Load assets
+bool ModuleSceneIntro::CleanUp()
+{
+	LOG("Unloading Intro scene");
+
+	return true;
+}
+
+// Update
+update_status ModuleSceneIntro::Update(float dt)
+{
+	Plane p(0, 1, 0, 0);
+	p.axis = true;
+	p.Render();
+
+	// TODO 4: update the transform of the shape to meet the
+	// physics one
+
+	return UPDATE_CONTINUE;
+}
+
+// TODO 5: ... and define it for the ModuleScenario. Set the ball
+// in red if it happens using is color property
+
+
